7e9eb232f189e849b96a4a326283bdc0d225fac8
Anton Belski <anton.belski@gmail.com>
2025-10-09 12:05:25 +0300

    feat: add solve for convertToRomanNumerals

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -122,8 +122,60 @@ function isIsoscelesTriangle(a, b, c) {
  *  10  => X
  *  26  => XXVI
  */
-function convertToRomanNumerals(/* num */) {
-  throw new Error('Not implemented');
+function convertToRomanNumerals(num) {
+  let res = '';
+  const tens = Math.floor(num / 10);
+  const ones = num % 10;
+  switch (tens) {
+    case 0:
+      break;
+    case 1:
+      res += 'X';
+      break;
+    case 2:
+      res += 'XX';
+      break;
+    case 3:
+      res += 'XXX';
+      break;
+    default:
+      break;
+  }
+  switch (ones) {
+    case 0:
+      break;
+    case 1:
+      res += 'I';
+      break;
+    case 2:
+      res += 'II';
+      break;
+    case 3:
+      res += 'III';
+      break;
+    case 4:
+      res += 'IV';
+      break;
+    case 5:
+      res += 'V';
+      break;
+    case 6:
+      res += 'VI';
+      break;
+    case 7:
+      res += 'VII';
+      break;
+    case 8:
+      res += 'VIII';
+      break;
+    case 9:
+      res += 'IX';
+      break;
+    default:
+      break;
+  }
+
+  return res;
 }
 
 /**
